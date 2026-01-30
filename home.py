0c268a89693ad858f797094bd921493a06f3c90b
unknown <abdihalykbanu@gmail.com>
2026-01-31 03:06:59 +0500

    Add Practice1 Python basics examples

---
+++ home.py
@@ -0,0 +1,14 @@
+# home1.py
+print("Hello, World!")
+
+# home2.py
+print("Python is fun!")
+
+# home3.py
+print(5 + 3)
+
+# home4.py
+print("Learning Python at W3Schools")
+
+# home5.py
+print("Welcome to Python Basics")
